68728da2647c2d4546c2666601e62a5b469235e9
Fruktion <106382628+Fruktion@users.noreply.github.com>
2022-05-31 22:08:05 +0200

    Add files via upload

--- src/zwierzeta/Jajko.java
+++ src/zwierzeta/Jajko.java
@@ -1,6 +1,10 @@
-package zwierzeta;
-
 public class Jajko {
     public Jajko(boolean _zaplodnione, long _czasWykluwania) {
+    }
+    kurczak zaktualizujWysiadywanie(long czas){
+        //TODO: not implemented yet
+    }
+    Jajko(boolean _zaplodnione, long czasWykluwania){
+        //TODO: not implemented yet
     }
 }
