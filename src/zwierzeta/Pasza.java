fa27d6f1bf7ce71a98fa32f698640c070e2151c1
Fruktion <106382628+Fruktion@users.noreply.github.com>
2022-05-31 12:42:41 +0200

    Add files via upload

---
+++ src/zwierzeta/Pasza.java
@@ -0,0 +1,18 @@
+package urzadzenia;
+
+public abstract class Pasza {
+    float kalorycznosc;
+    float masa;
+
+    public void Pasza(float _kalorycznosc, float _masa) {
+        kalorycznosc = _kalorycznosc;
+        masa = _masa;
+    }
+    point podziel(float podzial){
+        TODO: not implemented yet
+    }
+    Pasza(Pasza, Pasza){
+        TODO: not implemented yet
+    }
+
+}
